513d4f80cacee88366f93cf7fe4e5b9ef99cb7a7
Drew Farris <drew.farris@gmail.com>
2016-09-16 11:04:01 -0400

    Added example of the Truecase Annotator
     - see: http://stanfordnlp.github.io/CoreNLP/truecase.html

--- src/main/java/drew/corenlp/TruecaseExample.java
+++ src/main/java/drew/corenlp/TruecaseExample.java
@@ -1,21 +1,9 @@
 package drew.corenlp;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-
 import com.google.common.io.Files;
-
 import edu.stanford.nlp.dcoref.CorefChain;
 import edu.stanford.nlp.dcoref.CorefCoreAnnotations.CorefChainAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.*;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
@@ -25,21 +13,30 @@ import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
 import edu.stanford.nlp.util.CoreMap;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
 /** A simple corenlp example ripped directly from the Stanford CoreNLP website using text from wikinews. */
-public class SimpleExample {
+public class TruecaseExample {
 
   public static void main(String[] args) throws IOException {
     // creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and coreference resolution 
     Properties props = new Properties();
-    props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+    props.put("annotators", "tokenize, ssplit, pos, lemma, truecase");
     StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-    
+
     // read some text from the file..
     File inputFile = new File("src/test/resources/sample-content.txt");
-    String text = Files.toString(inputFile, Charset.forName("UTF-8"));
+    String input = Files.toString(inputFile, Charset.forName("UTF-8"));
+    String lcInput = input.toLowerCase(); // downcase everything.
 
-    // create an empty Annotation just with the given text
-    Annotation document = new Annotation(text);
+    // create an empty Annotation with just the downcased text.
+    Annotation document = new Annotation(lcInput);
 
     // run all Annotators on this text
     pipeline.annotate(document);
@@ -48,36 +45,66 @@ public class SimpleExample {
     // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
     List<CoreMap> sentences = document.get(SentencesAnnotation.class);
 
-    for(CoreMap sentence: sentences) {
+    // capture the true cased tokens for evaluation.
+    List<String> tcTokens = new ArrayList<String>();
+
+    System.out.println("------ begin truecase output -----");
+    for (CoreMap sentence : sentences) {
       // traversing the words in the current sentence
       // a CoreLabel is a CoreMap with additional token-specific methods
-      for (CoreLabel token: sentence.get(TokensAnnotation.class)) {
+      for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
+        // this is the text of the token
+        String text = token.get(TextAnnotation.class);
+        String trueCase = token.get(TrueCaseAnnotation.class);
+        String trueCaseText = token.get(TrueCaseTextAnnotation.class);
+        System.out.printf("input:%s state:%s output:%s\n", text, trueCase, trueCaseText);
+        tcTokens.add(trueCaseText);
+      }
+    }
+    System.out.println("------ end truecase otuput -----");
+
+
+    // create an empty Annotation with just the standard text.
+    document = new Annotation(input);
+
+    // run all Annotators on this text
+    pipeline.annotate(document);
+    sentences = document.get(SentencesAnnotation.class);
+
+    // capture the standard tokens for evaluation - note this assumes that
+    // the pipeline won't generate additional tokens for the same input.
+    List<String> stdTokens = new ArrayList<String>();
+
+    for (CoreMap sentence : sentences) {
+      // traversing the words in the current sentence
+      // a CoreLabel is a CoreMap with additional token-specific methods
+      for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
         // this is the text of the token
         String word = token.get(TextAnnotation.class);
-        // this is the POS tag of the token
-        String pos = token.get(PartOfSpeechAnnotation.class);
-        // this is the NER label of the token
-        String ne = token.get(NamedEntityTagAnnotation.class);
-        
-        System.out.println("word: " + word + " pos: " + pos + " ne:" + ne);
+        stdTokens.add(word);
       }
-
-      // this is the parse tree of the current sentence
-      Tree tree = sentence.get(TreeAnnotation.class);
-      System.out.println("parse tree:\n" + tree);
-
-      // this is the Stanford dependency graph of the current sentence
-      SemanticGraph dependencies = sentence.get(CollapsedCCProcessedDependenciesAnnotation.class);
-      System.out.println("dependency graph:\n" + dependencies);
     }
 
-    // This is the coreference link graph
-    // Each chain stores a set of mentions that link to each other,
-    // along with a method for getting the most representative mention
-    // Both sentence and token offsets start at 1!
-    Map<Integer, CorefChain> graph = 
-        document.get(CorefChainAnnotation.class);
-    
-  }
+    // compare the output of the tc and the original to see how well we've done
+    int match = 0;
+    int sz = tcTokens.size();
 
+    System.out.println("------ begin evaluation output -----");
+
+    for (int i=0; i < sz; i++) {
+      String tcToken = tcTokens.get(i);
+      String stdToken = stdTokens.get(i);
+      if (tcToken.equals(stdToken)) {
+        match++;
+      }
+      else {
+        System.out.printf("Truecase mismatch: input:'%s' output:'%s' @ %d\n", stdToken, tcToken, i);
+      }
+    }
+
+    float errorRate = ((float) sz - match) / sz;
+    System.out.println("Error Rate: " + errorRate);
+
+    System.out.println("------ end evaluation output -----");
+  }
 }
